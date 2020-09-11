7891225c4c541c9dc3469622e92a51ddc2cea525
YuviPanda <yuvipanda@gmail.com>
2020-09-11 20:51:25 +0530

    Add dynamic UI selector
    But only if ?next= isn't already set

---
+++ extra-assets/js/login.js
@@ -0,0 +1,22 @@
+function setInterface(interfaceUrl) {
+    let loginUrl = new URL($('#home').data('authenticator-login-url'), document.location.origin);
+    loginUrl.searchParams.set('next', '/hub/user-redirect/' + interfaceUrl)
+    $('#login-button').attr(
+        'href',
+        loginUrl.toString()
+    );
+}
+$(function() {
+    // if next query param is presentm just do nothing
+    const nextPresent = new URL(document.location).searchParams.get('next');
+    if (!nextPresent) {
+        setInterface($("input[name='interface']:checked").val());
+
+        $("input[name='interface']").change(function() {
+            if (this.checked) {
+                setInterface(this.value)
+            }
+        });
+    }
+
+})
